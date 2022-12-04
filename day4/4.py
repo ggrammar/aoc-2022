3c9c56d6df43695313a4d8ead34068211bfe8283
grant <grant@Emmas-MacBook-Air-2.local>
2022-12-04 15:37:42 -0600

    shorten simple boolean functions

--- day4/4.py
+++ day4/4.py
@@ -1,8 +1,8 @@
 import re
 
 def input_to_two_ranges(input):
-	a, b, c, d = re.split(',|-', input)
-	return range(int(a), int(b) + 1), range(int(c), int(d) + 1)
+	a, b, c, d = map(int, re.split(',|-', input))
+	return range(a, b + 1), range(c, d + 1)
 
 def contains(first, second):
 	if (len(first) == 1) and (first in second):
@@ -12,16 +12,14 @@ def contains(first, second):
 		if first[-1] <= second[-1]:
 			return True
 
-def bidirectional_contains_on_two_ranges(ranges):
-	if contains(ranges[0], ranges[1]) or contains(ranges[1], ranges[0]):
-		return True
 	return False
 
+def bidirectional_contains_on_two_ranges(ranges):
+	return contains(ranges[0], ranges[1]) or contains(ranges[1], ranges[0])
+
 def overlaps_on_two_ranges(ranges):
-	for i in ranges[0]:
-		if i in ranges[1]:
-			return True
-	return False
+	# only set has the 'intersection' method, but it accepts any iterable as an argument.
+	return any(set(ranges[0]).intersection(ranges[1]))
 
 with open("./input.txt", "r") as f:
 	ranges = map(input_to_two_ranges, f.readlines())
