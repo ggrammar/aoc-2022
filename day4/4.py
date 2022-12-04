5c50a84b044a1c6d3d2c742b6c2b9964d3a88f1d
grant <grant@Emmas-MacBook-Air-2.local>
2022-12-04 16:11:37 -0600

    better use of map and lambda

--- day4/4.py
+++ day4/4.py
@@ -1,16 +1,9 @@
 import re
 
-input = map(
-	lambda line : map(int, re.split(',|-', line)), 
-	open("./input.txt")
-)
+input = map(lambda line : map(int, re.split(',|-', line)), open("./input.txt"))
 
-def contains(a):
-	return (a[0] >= a[2] and a[1] <= a[3]) or (a[0] <= a[2] and a[1] >= a[3])
+contains = lambda a : (a[0] >= a[2] and a[1] <= a[3]) or (a[0] <= a[2] and a[1] >= a[3])
+overlaps = lambda a : any(set(range(a[0], a[1] + 1)).intersection(range(a[2], a[3] + 1)))
 
-def overlaps(a):
-	return any(set(range(a[0], a[1] + 1)).intersection(range(a[2], a[3] + 1)))
-
-print(sum(map(contains, input)))
-print(sum(map(overlaps, input)))
+print("{}, {}".format( sum(map(contains, input)), sum(map(overlaps, input) )))
 
