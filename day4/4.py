d11186e8f831aaef96e6477eff1fc4f7e2e9a38d
grant <grant@Emmas-MacBook-Air-2.local>
2022-12-04 16:21:04 -0600

    concession to readability

--- day4/4.py
+++ day4/4.py
@@ -1,9 +1,16 @@
 import re
 
-input = map(lambda line : map(int, re.split(',|-', line)), open("./input.txt"))
+input = map(
+	lambda line : map(int, re.split(',|-', line)), 	
+	open("./input.txt")
+)
 
-contains = lambda a : (a[0] >= a[2] and a[1] <= a[3]) or (a[0] <= a[2] and a[1] >= a[3])
+contains = lambda a : (
+	a[0] >= a[2] and a[1] <= a[3]
+	or a[0] <= a[2] and a[1] >= a[3]
+)
+
 overlaps = lambda a : any(set(range(a[0], a[1] + 1)).intersection(range(a[2], a[3] + 1)))
 
-print("{}, {}".format( sum(map(contains, input)), sum(map(overlaps, input) )))
+print("{}, {}".format(sum(map(contains, input)), sum(map(overlaps, input))))
 
